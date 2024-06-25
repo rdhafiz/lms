58fd0dad773254a7e545ac201e7b73ab0816db1f
Ridwanul Hafiz <ridwanul.hafiz@gmail.com>
2024-06-25 10:35:20 +0600

    `complete-get-in-touch-with-us`

--- resources/views/front/pages/contact/contact.blade.php
+++ resources/views/front/pages/contact/contact.blade.php
@@ -31,6 +31,101 @@
         </div>
     </section>
 
+    {{-- Get in touch with us --}}
+    <section class="w-100 py-5">
+        <div class="container py-5">
+            <div class="mb-4 fw-bold text-center fs-1">
+                Get In Touch With Us
+            </div>
+            <div class="row row-cols-1 row-cols-sm-1 row-cols-md-1 row-cols-lg-2">
+                <div class="p-3">
+                    <div
+                        class="shadow p-2 p-sm-5 bg-size-cover object-fit-cover bg-repeat-none d-flex align-items-center justify-content-start"
+                        style="background-image: url({{asset('/images/about/small-abstract-spheres.jpg')}})">
+                        <div class="me-3">
+                            <div
+                                class="width-65 height-65 d-flex justify-content-center align-items-center bg-light-info rounded-circle">
+                                <img src="{{asset('/images/svg/about/geo-alt.svg')}}" class="width-25 height-25"
+                                     alt="geo-alt">
+                            </div>
+                        </div>
+                        <div>
+                            <div class="d-block fw-bold text-theme-secondary">
+                                Our Address
+                            </div>
+                            <div class="text-secondary">
+                                32D, Jenmark road, Franklin, USA
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="p-3">
+                    <div
+                        class="shadow p-2 p-sm-5 bg-size-cover object-fit-cover bg-repeat-none d-flex align-items-center justify-content-start"
+                        style="background-image: url({{asset('/images/about/small-abstract-spheres.jpg')}})">
+                        <div class="me-3">
+                            <div
+                                class="width-65 height-65 d-flex justify-content-center align-items-center bg-light-pink rounded-circle">
+                                <img src="{{asset('/images/svg/about/telephone.svg')}}" class="width-25 height-25"
+                                     alt="telephone">
+                            </div>
+                        </div>
+                        <div>
+                            <div class="d-block fw-bold text-theme-secondary">
+                                Contact
+                            </div>
+                            <div class="text-secondary">
+                                +1(135) 1984 2020
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="p-3">
+                    <div
+                        class="shadow p-2 p-sm-5 bg-size-cover object-fit-cover bg-repeat-none d-flex align-items-center justify-content-start"
+                        style="background-image: url({{asset('/images/about/small-abstract-spheres.jpg')}})">
+                        <div class="me-3">
+                            <div
+                                class="width-65 height-65 d-flex justify-content-center align-items-center bg-light-purple rounded-circle">
+                                <img src="{{asset('/images/svg/about/clock.svg')}}" class="width-25 height-25"
+                                     alt="clock">
+                            </div>
+                        </div>
+                        <div>
+                            <div class="d-block fw-bold text-theme-secondary">
+                                House of Operations
+                            </div>
+                            <div class="text-secondary">
+                                (8 AM - 7 PM, Monday - Friday)
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="p-3">
+                    <div
+                        class="shadow p-2 p-sm-5 bg-size-cover object-fit-cover bg-repeat-none d-flex align-items-center justify-content-start"
+                        style="background-image: url({{asset('/images/about/small-abstract-spheres.jpg')}})">
+                        <div class="me-3">
+                            <div
+                                class="width-65 height-65 d-flex justify-content-center align-items-center bg-secondary rounded-circle">
+                                <img src="{{asset('/images/svg/about/envelope.svg')}}" class="width-25 height-25"
+                                     alt="clock">
+                            </div>
+                        </div>
+                        <div>
+                            <div class="d-block fw-bold text-theme-secondary">
+                                Email
+                            </div>
+                            <div class="text-secondary">
+                                example@example.com
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
 
 
 @endsection
