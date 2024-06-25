1e834980f462f88cba14348651fe53a106d78989
Ridwanul Hafiz <ridwanul.hafiz@gmail.com>
2024-06-25 11:10:55 +0600

    'title-change-into-courses'

--- resources/views/front/pages/course/course.blade.php
+++ resources/views/front/pages/course/course.blade.php
@@ -8,7 +8,7 @@
         <div class="container">
             <div class="d-flex justify-content-start align-items-center height-350">
                 <div>
-                    <div class="fs-1 fw-bold text-theme-secondary mb-3 d-block"> About Us </div>
+                    <div class="fs-1 fw-bold text-theme-secondary mb-3 d-block"> Courses </div>
 
                     <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
                         <ol class="breadcrumb">
