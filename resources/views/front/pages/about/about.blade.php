96d616f275147383a6ad08b120170a3d38c43289
Ridwanul Hafiz <ridwanul.hafiz@gmail.com>
2024-06-25 09:43:46 +0600

    `complete-about-section`

--- resources/views/front/pages/about/about.blade.php
+++ resources/views/front/pages/about/about.blade.php
@@ -29,4 +29,43 @@
         </div>
     </section>
 
+    {{-- welcome to our online platform --}}
+    <section class="w-100 bg-white py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 p-2 p-lg-5">
+                    <div class="position-relative">
+                        <div class="position-absolute start-0 bottom-0 p-3 d-none d-lg-inline">
+                            <img src="{{asset('/images/about/about-top-left.jpg')}}" class="img-fluid width-250 border border-white border-5 shadow rounded-4" alt="about-top-left">
+                        </div>
+                        <img src="{{asset('/images/about/about-cover.jpg')}}" class="img-fluid rounded-4" alt="about-cover">
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="fs-1 fw-bold text-theme-secondary mb-3"> Welcome to Our Online Learning Platform </div>
+                    <div class="lead mb-4">
+                        Continuously harmless backward competitive initiatives and synergistic content.
+                        Objectively strategize cutting edge niches with collaborative synergistic.
+                        Globally pontificate e-business processes through orthoonal web readiness enhence
+                        backend value whereas bricks-and-clicks services
+                    </div>
+                    <div class="mb-3 d-flex align-items-center justify-content-start lead">
+                        <img src="{{asset('/images/svg/about/check-circle.svg')}}" class="width-21 height-21 me-2" alt="check-circle">
+                        Dramatically re-engineer value added systems via mission
+                    </div>
+                    <div class="mb-3 d-flex align-items-center justify-content-start lead">
+                        <img src="{{asset('/images/svg/about/check-circle.svg')}}" class="width-21 height-21 me-2" alt="check-circle">
+                        Our Unique learning environment sparks physical growth
+                    </div>
+                    <div class="mb-3 d-flex align-items-center justify-content-start lead">
+                        <img src="{{asset('/images/svg/about/check-circle.svg')}}" class="width-21 height-21 me-2" alt="check-circle">
+                        Discovery while our creative curriculum which combines
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
 @endsection
