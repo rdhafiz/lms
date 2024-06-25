5d8bf1a642aabb6aaf62832dea444a36e871cec3
Ridwanul Hafiz <ridwanul.hafiz@gmail.com>
2024-06-25 11:06:39 +0600

    'complete-breadcrumb-courses'

--- resources/views/front/pages/course/course.blade.php
+++ resources/views/front/pages/course/course.blade.php
@@ -3,6 +3,32 @@
 
 @section('content')
 
+    {{-- breadcrumb --}}
+    <section class="w-100 bg-warning-subtle">
+        <div class="container">
+            <div class="d-flex justify-content-start align-items-center height-350">
+                <div>
+                    <div class="fs-1 fw-bold text-theme-secondary mb-3 d-block"> About Us </div>
+
+                    <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item">
+                                <a href="{{route('front.home')}}" class="text-decoration-none text-dark">
+                                    Home
+                                </a>
+                            </li>
+                            <li class="breadcrumb-item active">
+                                <a href="{{route('front.courses')}}" class="text-decoration-none text-theme">
+                                    Courses
+                                </a>
+                            </li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </section>
+
 
 
 @endsection
