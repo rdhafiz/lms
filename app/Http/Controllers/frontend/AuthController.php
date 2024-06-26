85a0aef0b0becde9f958bd343c35755c29c40e3d
Ridwanul Hafiz <ridwanul.hafiz@gmail.com>
2024-06-26 11:48:58 +0600

    remove-dd()-related-code-from-auth-controller

--- app/Http/Controllers/frontend/AuthController.php
+++ app/Http/Controllers/frontend/AuthController.php
@@ -16,74 +16,46 @@ class AuthController extends BaseController
 {
 
     public function login(Request $request) {
-
         try {
-
             $validator = Validator::make($request->all(), [
                 'email' => 'required',
                 'password' => 'required',
             ]);
-
             if($validator->fails()) {
                 return response()->json(['errors' => $validator->errors()], 422);
             }
-
-//            dd($validator);
-
             $credential = ['email' => $request->email, 'password' => $request->password];
 
-//            dd($credential);
-
             if(Auth::guard('users')->attempt($credential)) {
-
-//                dd(Auth::guard('users'));
-
                 return response()->json(['message' => 'Login successfully', 'data' => Auth::guard('users')->user()], 200);
-
             } else {
-
-//                dd(response()->json());
-
                 return response()->json(['status' => 500, 'errors' => ['error' => 'Invalid Credentials! Please try again']], 500);
-
             }
-
         }catch ( \Exception $exception ) {
-
             return response()->json(['status' => 500, 'errors' => $exception->getMessage(), 'line' => $exception->getLine()], 500);
-
         }
 
     }
 
     public function register(Request $request) {
-
         try {
-
             $validator = Validator::make($request->all(), [
                 'name' => 'required',
                 'email' => 'required|unique:users,email',
                 'password' => 'required|min:3|confirmed'
             ]);
-
             if($validator->fails()) {
                 return response()->json(['errors' => $validator->errors()], 422);
             }
-
             $user = new User();
             $user->name = $request->name;
             $user->email = $request->email;
             $user->password = bcrypt($request->password);
             $user->save();
-
             return response()->json(['message' => 'Registration has been complete successfully']);
-
         }catch ( \Exception $exception ) {
-
             return response()->json(['status' => 500, 'errors' => $exception->getMessage(), 'line' => $exception->getLine()], 500);
-
         }
-
     }
 
     public function forget() {
